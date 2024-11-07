da3f44ca9eff33600888116d8913fb0f03d576b7
Diego-Ortiz-Ostolaza <diego.ortiz.joseph@gmail.com>
2024-11-07 10:02:45 -0500

    TRABAJO

---
+++ desayunos/desayunos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desayunos
+{
+    internal class desayunos
+    {
+
+        
+        
+            public String Nombre { get; set; }
+            public Double Precio { get; set; }
+            public String Dias { get; set; }
+
+            public override string ToString()
+            {
+                return $"Desayuno {Nombre}, servido los {Dias} a S/{Precio}.";
+            }
+            public string[] ListaDias()
+            {
+                return Dias.Split(';');
+            
+        }
+}
