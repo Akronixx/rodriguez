d2b7ec30a32b39332e207cdd574f61af8e799338
KingLeonYT <tannersrn1yt@gmail.com>
2024-11-13 22:29:41 -0500

    fin-de-crear

---
+++ desayunos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desayunos
+{
+    public class creard
+    {
+            string[] Desayuno1 = new string[0];
+            int posicion = 0;
+  
+            public void crear()
+            {
+                    string Respuesta;
+                    Console.Write("Ingresa el nombre del desayuno : ");
+                    Respuesta = Console.ReadLine();
+                    Array.Resize(ref Desayuno1, Desayuno1.Length + 1);
+                    Desayuno1[posicion] = Respuesta;
+                    posicion=posicion+1;
+                    Console.WriteLine("A agregado un desayuno ");
+            }
+    }
+}
